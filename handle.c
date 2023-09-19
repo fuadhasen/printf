d5d0375ca53cf4ddd5c5676940936c094935c500
kedabdure <abdurehimk77@gmail.com>
2023-09-19 02:04:29 -0700

    committed

--- handle.c
+++ handle.c
@@ -25,3 +25,29 @@ int _printf(const char *format, ...)
     va_end(args);
     return (i);
 }
+int print_number(int n)
+{
+    int count = 0;
+    unsigned int num;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        num = -n;
+        count++;
+    }
+    else
+    {
+        num = n;
+    }
+
+    if (num / 10 != 0)
+    {
+        count += print_number(num / 10);
+    }
+
+    _putchar('0' + (num % 10));
+    count++;
+
+    return (count);
+}
