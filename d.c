97462effb6a55894aa22c15ce1028a7923eeac16
fuadhasen <fuadhas6634@gmail.com>
2023-09-19 04:01:12 -0700

    f

--- d.c
+++ d.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * _p - Custom printf function
@@ -37,36 +36,5 @@ int _p(const char *format, ...)
 	}
 	va_end(args);
 	return (i);
-}
-
-/**
- * print_number - Custom function to print an integer.
- * @n: The integer to be printed.
- *
- * Return: The number of characters printed.
- */
-int print_number(int n)
-{
-	int count = 0;
-	unsigned int num;
-
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		num = -n;
-		count++;
-	}
-	else
-	{
-		num = n;
-	}
-	if (num / 10 != 0)
-	{
-		count += print_number(num / 10);
-	}
-	_putchar((char)('0' + (num % 10)));
-	count++;
-
-	return (count);
 }
 
---
+++ print.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_number - Custom function to print an integer.
+ * @n: The integer to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int print_number(int n)
+{
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+		count++;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10 != 0)
+	{
+		count += print_number(num / 10);
+	}
+	_putchar((char)('0' + (num % 10)));
+	count++;
+	return (count);
+}
